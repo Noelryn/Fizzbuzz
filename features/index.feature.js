5efe7c3152c5eaf982f65a63df8041fbe9bf2cff
Noel Ryan <noelgerard.ryan@gmail.com>
2018-09-08 19:19:16 +0200

    added code to the index, debugged and all tests are passing

--- features/index.feature.js
+++ features/index.feature.js
@@ -13,12 +13,15 @@ describe('User can input a value and get FizzBuzz results', () => {
     after(async ()=> {
         await browser.close();
     })
+
+    it('clicking on the "Check" button', async () => {
+        await browser.fillIn("input[id='value']", { with: "3" })
+        await browser.clickOnButton("input[value='Check']")
+        let content = await browser.getContent("[id='display_answer']")
+        expect(content).to.eql('Fizz');
+    })
+
+    
 })
 
-it('clicking on the "Check" button', async () => {
-    await browser.fillIn("input[id='value']", { with: "3" })
-    await browser.clickOnButton("input[value='Check']")
-    let content = await browser.getContent("[id='display_answer']")
-    expect(content).to.eql('Fizz');
-})
 
