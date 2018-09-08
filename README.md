666f626aa279df546772a78ad8fed47cec32079a
Noel Ryan <noelgerard.ryan@gmail.com>
2018-09-08 14:22:54 +0200

    added added in new funictions into js file and new test into specs file 2 tests passing

---
+++ README.md
@@ -0,0 +1,8 @@
+Question 1. 
+
+let  fizzBuzz = fs.readFileSync('./src/js/fizz-buzz.js');
+ eval( fizzBuzz + `\nexports.FizzBuzz = FizzBuzz;`)
+
+ I think it is saying to let fizzbuzz file sync with the fizzbuzz Javascript file in the source folder.
+
+
---
+++ src/js/fizz-buzz.js
@@ -0,0 +1,15 @@
+function FizzBuzz() {
+    this.check = (number) => {
+        return number
+    }
+}
+
+function FizzBuzz() {
+    this.check = (number) => {
+        if (number % 3 === 0) {
+            return 'Fizz';
+        } else {
+            return number 
+        }
+    }
+}
