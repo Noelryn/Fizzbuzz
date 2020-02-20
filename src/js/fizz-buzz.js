ed4aad1a512dc52bbb2e590abb983bea44a9d9cd
Noel Ryan <noelgerard.ryan@gmail.com>
2020-02-20 13:48:32 +0100

    changing message that shows on screen

--- src/js/fizz-buzz.js
+++ src/js/fizz-buzz.js
@@ -1,34 +1,8 @@
 function FizzBuzz() {
     this.check = (number) => {
-        return number
-    }
-}
-
-function FizzBuzz() {
-    this.check = (number) => {
-        if (number % 3 === 0) {
-            return 'Fizz';
-        } else {
-            return number 
-        }
-    }
-}
-
-function FizzBuzz() {
-    this.check = (number) => {
-        if (number % 5 === 0) {
-            return 'Buzz';
-        } else if (number % 3 === 0) {
-            return 'Fizz';
-        } else {
-            return number
-        }
-    }
-}
-
-function FizzBuzz() {
-    this.check = (number) => {
-        if (number % 15 === 0) {
+        if (number  === '' ) {
+        return 'PLEASE ADD A NUMBER DUMBASS';  
+        } else if (number % 15 === 0) {
             return 'FizzBuzz';
         } else if (number % 5 === 0) {
             return 'Buzz';
